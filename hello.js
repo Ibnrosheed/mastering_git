d43083950b47dc4d23ac943a79e44be2f3ee73a9
ibnrosheed <ibnrosheed>
2026-03-16 12:58:37 +0100

    revert commit

--- hello.js
+++ hello.js
@@ -1,7 +1,6 @@
-    console.log("Hello, git");
+console.log("Hello, git");
 console.log('Hello, git from dev-eng');
 console.log('Hello, git from dev-eng 2');
 console.log('Hello, git from dev-eng 3');
 console.log('Hello, git from dev-eng 4');
-console.log('This conflict is solved');
-console.log('Hello, git trying a revert');
+console.log('This conflict is solved')
