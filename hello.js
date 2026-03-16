6322e521839f86da53cd6300e95b996172d55172
ibnrosheed <ibnrosheed>
2026-03-16 12:51:49 +0100

    add a revert to hello.js

--- hello.js
+++ hello.js
@@ -1,6 +1,7 @@
-console.log("Hello, git");
+    console.log("Hello, git");
 console.log('Hello, git from dev-eng');
 console.log('Hello, git from dev-eng 2');
 console.log('Hello, git from dev-eng 3');
 console.log('Hello, git from dev-eng 4');
-console.log('This conflict is solved')
+console.log('This conflict is solved');
+console.log('Hello, git trying a revert');
